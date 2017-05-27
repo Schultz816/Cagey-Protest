16894cb21ea7410a370692e1686d7202cc35c931
JPSauSara <jpsausara@users.noreply.github.com>
2017-05-27 14:16:33 -0500

    :add: development files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Cagey-Protest
+
+## JPS - Julia P. Saucedo (@JPSauSara)
+
+This branch is focused on the rewards section of the Cagey-Protest project 
+
+:trophy:
--- public/assets/js/rewards.js
+++ public/assets/js/rewards.js
@@ -1,10 +1,39 @@
 $(document).ready(function () {
+  // Getting references to the name input and author container, as well as the table body
+	var $rewardInput = $("#reward-name");
+	var $rewardList = $("tbody");
+	var $rewardContainer = ("#reward-container");
 
-$(document).on("submit", "#reward-form", handleRewardFormSubmit);
+	// Adding event listeners to the form to create a new object, and the button to delete a reward
+	$(document).on("submit", "#reward-form", handleRewardFormSubmit);
+	// $(document).on("click", ".delete-reward", handleDeleteButtonPress);
 
-function handleRewardFormSubmit() {
-	alert("working too!");
-};
+	// Getting the initial list of Rewards
+	// getRewards();
+
+	// A function to handle what happens when the form is submitted to create a new Reward
+	function handleRewardFormSubmit(event) {
+		alert("working too!");
+		event.preventDefault();
+		// Don't do anything if the reward fields haven't been filled out
+		if (!$rewardInput.val().trim().trim()) {
+			return;
+			alert("dude, you didn't complete the fields. Try again");
+		} else {
+			// Calling the upsertReward function and passing in the value of the $rewardInput
+			upsertReward({
+				name: $rewardInput.val().trim()
+			});
+		};
+
+		// A function for creating a reward. Calls getRewards upon completion
+		function upsertReward(rewardData) {
+
+		};
+
+
+
+	};
 
 $(".btn").on("click", function(){
 	alert("maybe working?");
