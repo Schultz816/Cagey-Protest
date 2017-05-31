540c7bb3a1d97dbfef57a85c2abbe09a0788c437
Marc Deheeger <mcdeheeger@Marcs-MacBook-Pro.local>
2017-05-31 06:12:20 -0500

    committing julia's most recent chanes

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
 // Routes =============================================================
 
 require("./routes/html-routes.js")(app);
