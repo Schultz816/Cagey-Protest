e8ce97cb816baf45c88c6bd08b7bae5a153e1699
JPSauSara <jpsausara@users.noreply.github.com>
2017-05-30 20:40:28 -0500

    merge miguel

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
 // *****************************************************************************
 // Server.js - This file is the initial starting point for the Node/Express server.
 //
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-=======
->>>>>>> ca1493373a7b4f2bf64dbc41fb79f856bb7d3876
 const express = require("express");
 const bodyParser = require("body-parser");
-// const expressHandlebars = require("express-handlebars");
-// const methodOverride = require("method-override"); // might not need
-const passport = require('passport');
-const session = require('express-session');
+const expressHandlebars = require("express-handlebars");
+//const methodOverride = require("method-override"); // might not need
 
 // Sets up the Express App
 // =============================================================
@@ -31,48 +26,21 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Static directory
 app.use(express.static("./public"));
 
-<<<<<<< HEAD
 // <<<<<<< HEAD
 // Override with POST having ?_method=DELETE
 //app.use(methodOverride("_method")); // might not use
 
 // Set Handlebars =============================================================
 var exphbs = require("express-handlebars");
-=======
-// Override with POST having ?_method=DELETE
-//app.use(methodOverride("_method")); // might not use
 
-// Set Handlebars.
-// var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-// For Passport
-app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-
->>>>>>> ca1493373a7b4f2bf64dbc41fb79f856bb7d3876
-
-// path doesn't exist yet nor does auth.js route
-// //load passport strategies
-// require('./config/passport/passport.js')(passport, models.user);
-
-<<<<<<< HEAD
 // Routes =============================================================
 
 require("./routes/html-routes.js")(app);
 require("./routes/reward-api-routes.js")(app);
 
-=======
-// Routes - not set up yet =============================================================
-// var authRoute = require('./routes/auth.js')(app,passport);
-
-require("./routes/html-routes.js")(app);
-require("./routes/reward-api-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
->>>>>>> ca1493373a7b4f2bf64dbc41fb79f856bb7d3876
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({ force: true}).then(function() {
