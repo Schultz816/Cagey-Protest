69afd8b27a3685b68489721b70e9af3d54b3e34e
Marc Deheeger <mcdeheeger@Marcs-MacBook-Pro.local>
2017-05-30 22:56:31 -0500

    update: final passport files added

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -14,8 +14,27 @@ module.exports = function(app, passport) {
 
     ));
 
-    app.get('/dashboard',authController.dashboard);
+    app.get('/dashboard', isLoggedIn, authController.dashboard);
 
     app.get('/logout',authController.logout);
+
+
+    app.post('/signin', passport.authenticate('local-signin', {
+            successRedirect: '/dashboard',
+
+            failureRedirect: '/signin'
+        }
+
+    ));
+
+    function isLoggedIn(req, res, next) {
+
+        if (req.isAuthenticated())
+
+            return next();
+
+        res.redirect('/signin');
+
+    }
 
 }
