dfa7d213ba70418d2dd6f01a04456ac19b45355e
JPSauSara <jpsausara@users.noreply.github.com>
2017-05-31 20:48:23 -0500

    bug: err empty response api/rewards

--- routes/reward-api-routes.js
+++ routes/reward-api-routes.js
@@ -1,35 +1,36 @@
  // Created by Julia on 5/28/2017.
 
-var Reward = require("../models/reward.js");
+var db = require("../models");
 
 module.exports = function(app) {
 	app.get("/api/rewards", function(req, res) {
-		Reward.findAll({
-			// include: [db.Post]
+		db.rewards.findAll({
+			include: [db.Post]
 		}).then(function(dbReward) {
 			res.json(dbReward);
 		});
+		// res.send("reward-api is working!");
 	});
 
 	app.get("/api/rewards/:id", function(req, res) {
-		Reward.findOne({
+		db.rewards.findOne({
 			where: {
 				id: req.params.id
 			},
-			// include: [db.Post]
+			include: [db.Post]
 		}).then(function(dbReward) {
 			res.json(dbReward);
 		});
 	});
 
 	app.post("/api/rewards", function(req, res) {
-		Reward.create(req.body).then(function(dbReward) {
+		db.rewards.create(req.body).then(function(dbReward) {
 			res.json(dbReward);
 		});
 	});
 
 	app.delete("/api/rewards/:id", function(req, res) {
-		Reward.destroy({
+		db.rewards.destroy({
 			where: {
 				id: req.params.id
 			}
