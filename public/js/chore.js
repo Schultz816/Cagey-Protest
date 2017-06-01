6dca5c0afba8be18b8ff74682047e5f9b02c1ff3
Corey Schultz <Corey.w.schultz@gmail.com>
2017-05-31 19:16:57 -0500

    today

--- public/js/chore.js
+++ public/js/chore.js
@@ -1,24 +1,31 @@
-var topics = ["Wash Dishes", "Clean Room", "Laundry", "Vaccum", "Walk the Dog", "Clean Bathroom", "Water Plants",];
-// creates buttons from above var
-installAnimalButtons();
+var chore = ["Mow Lawn", "Wash Dishes", "Clean Room", "Laundry", "Vaccum", "Walk the Dog", "Clean Bathroom", "Water Plants"];
+function firstMethod(){
+	for (var i = 0; i < chore.length; i++) {
+		/*var choreBtn = $("<button>");
+		choreBtn.addClass("btn btn-primary btn-lg btn-block");
+		choreBtn.attr("data-chore", chore[i]);
+		choreBtn.text(chore[i]);
+		$("#buttons").append(choreBtn);*/
+		//alert(chore[i]);
+		var $input = $('<input type="button" class="btn btn-primary btn-lg btn-block"  onclick="call(this)" value="'+chore[i]+' +100" />');
+		$input.appendTo($("#column1"));
+	}
+	
+}
 
+function call(name){
+	//alert("added");
+	var $input = $('<input type="button" class="btn btn-primary btn-lg btn-block"  value="'+name.value+'" />');
+    $input.appendTo($("#column2"));
+}
 
-$('#addAnimal').on('click', function() {
-    var animalEntered = $('#animalInput').val().trim();
-    topics.push(animalEntered);
-    $('#animalInput').val('');
-    installAnimalButtons();
-
-    return false;
-});
-
-
-function installAnimalButtons() {
-     $('#animalButtons').empty();
-
-     for (var i = 0; i < topics.length; i++) {
-         var button = $('<button>').addClass('btn btn-primary btn-lg btn-block');
-         button.attr('data-animal', topics[i]).html(topics[i]);
-         $('#animalButtons').append(button);
-    }
+function createInput(){
+		var buttonName = document.getElementById("inlineFormInput").value;
+		var pointValue = document.getElementById("inlineFormInput1").value;
+		//alert("inside createInput"+pointValue+" name :"+buttonName);
+		chore.push(buttonName);
+		buttonName = buttonName+"&emsp;+";
+		var $input = $('<input type="button" class="btn btn-primary btn-lg btn-block" onclick="call(this)"  value="'+buttonName+pointValue+'" />');
+        $input.appendTo($("#column1"));
+		
 }
---
+++ public/js/chore1.js
@@ -0,0 +1,31 @@
+var chore = ['Example'];
+function firstMethod(){
+	for (var i = 0; i < chore.length; i++) {
+		/*var choreBtn = $("<button>");
+		choreBtn.addClass("btn btn-primary btn-lg btn-block");
+		choreBtn.attr("data-chore", chore[i]);
+		choreBtn.text(chore[i]);
+		$("#buttons").append(choreBtn);*/
+		//alert(chore[i]);
+		var $input = $('<input type="button" class="btn btn-primary btn-lg btn-block"  onclick="call(this)" value="'+chore[i]+' +100" />');
+		$input.appendTo($("#column1"));
+	}
+	
+}
+
+function call(name){
+	//alert("added");
+	var $input = $('<input type="button" class="btn btn-primary btn-lg btn-block"  value="'+name.value+'" />');
+    $input.appendTo($("#column2"));
+}
+
+function createInput(){
+		var buttonName = document.getElementById("inlineFormInput").value;
+		var pointValue = document.getElementById("inlineFormInput1").value;
+		//alert("inside createInput"+pointValue+" name :"+buttonName);
+		chore.push(buttonName);
+		buttonName = buttonName+"&emsp;+";
+		var $input = $('<input type="button" class="btn btn-primary btn-lg btn-block" onclick="call(this)"  value="'+buttonName+pointValue+'" />');
+        $input.appendTo($("#column1"));
+		
+}
