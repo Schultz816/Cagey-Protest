d85cfb997f6c2740517ec2a1f5508878e53787a5
JPSauSara <jpsausara@users.noreply.github.com>
2017-06-02 21:16:14 -0500

    styling additions

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -1,30 +1,30 @@
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
-//
-// Dependencies
-// =============================================================
-var path = require("path");
+// // *********************************************************************************
+// // html-routes.js - this file offers a set of routes for sending users to the various html pages
+// // *********************************************************************************
+// //
+// // Dependencies
+// // =============================================================
+// var path = require("path");
 
-// Routes
-// =============================================================
-module.exports = function(app) {
+// // Routes
+// // =============================================================
+// module.exports = function(app) {
 
-    // Each of the below routes just handles the HTML page that the user gets sent to.
+//     // Each of the below routes just handles the HTML page that the user gets sent to.
 
-    // index route loads view.html
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/login.html"));
-    });
-
-
-    app.get("/reward", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/reward.html"));
-    });
-
-    app.get("/home", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
+//     // index route loads view.html
+//     app.get("/", function(req, res) {
+//         res.sendFile(path.join(__dirname, "../public/login.html"));
+//     });
 
 
-};
+//     app.get("/reward", function(req, res) {
+//         res.sendFile(path.join(__dirname, "../public/reward.html"));
+//     });
+
+//     app.get("/home", function(req, res) {
+//         res.sendFile(path.join(__dirname, "../public/home.html"));
+//     });
+
+
+// };
